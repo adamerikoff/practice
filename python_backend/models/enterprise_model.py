2b7d26e4adb66d1cb58abadfca4ac51d77bb45b9
adamerikoff <adamerikoff@protonmail.com>
2024-03-19 03:40:13 +0500

    added relationship between Enterprises and Products

--- python_backend/models/enterprise_model.py
+++ python_backend/models/enterprise_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Float
 from sqlalchemy.orm import relationship
 
 from ..database import Base
@@ -13,8 +13,26 @@ class Enterprise(Base):
     location = Column(String, default='')
     business_hours = Column(String, default='9:00 - 18:00')
 
+    products = relationship("Product", back_populates="enterprise", uselist=True)
+
     def __repr__(self):
         return f"<Enterprise(id={self.id}, name='{self.name}', description='{self.description}', location='{self.location}', business_hours='{self.business_hours}')>"
 
-    
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, nullable=False)
+    description = Column(String, default='')
+    price = Column(Float, nullable=False)
+    quantity_in_stock = Column(Integer, nullable=False)
+
+    enterprise_id = Column(Integer, ForeignKey('enterprises.id'))
+
+    enterprise = relationship("Enterprise", back_populates="products")
+
+    def __repr__(self):
+        return f"<Product(id={self.id}, name='{self.name}', description='{self.description}', price={self.price}, quantity_in_stock={self.quantity_in_stock})>"
+
 
